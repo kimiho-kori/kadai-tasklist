3e6bc4d494861c498d5a4c21eb8ef801087bd4e1
kimiho-kori <emiryunsin@gmail.com>
2020-09-26 02:05:31 +0000

    15 kadai-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,8 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :correct_user, only: [:show, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   def index
-    
     @tasks = current_user.tasks.order(id: :desc)
   end
 
